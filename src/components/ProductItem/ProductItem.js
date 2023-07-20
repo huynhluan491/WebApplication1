605de3e5a3a66cf8cd5a018ecc74509d663525c5
THANHBINH\thanh <thanhbinh072004@gmail.com>
2023-07-20 23:42:30 +0700

    component item

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -18,19 +18,47 @@ function ProductItem({ data }) {
         dispatch({ type: 'ADD_TO_CART', payload: { product: data, quantity: 1 } });
     };
 
-    const handleButton = () => { //thông báo khi thêm sản phẩm vào giỏ hàng
+    const handleButton = () => {
         addToCart();
         showToast();
     };
 
     return (
         <div className={cx('wrap-product')}>
+            <div className={cx('item-product')}>
+                <div className={cx('add-btn')}>
+                    <Button addToCart smallTango onClick={() => handleButton()} />
+                    <ToastContainer hideProgressBar={true} />
+                </div>
+                <Link to={`/product/${data.name}`} onClick={() => window.scrollTo(0, 0)}>
+                    <div className={cx('product-img')}>
+                        <img src={data.image} alt="" />
+                    </div>
+                    <div className={cx('product-detail')}>
+                        <p className={cx('product-name')}>{data.name}</p>
+                        <h2 className={cx('product-price')}>
+                            {data.price.toLocaleString('it-IT', {
+                                style: 'currency',
+                                currency: 'VND',
+                            })}
+                        </h2>
+                        <p className={cx('sale-price')}>
+                            {(data.price - (data.price * 6) / 100).toLocaleString('it-IT', {
+                                style: 'currency',
+                                currency: 'VND',
+                            })}
+                        </p>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
 
 ProductItem.propTypes = {
+
     data: PropTypes.object.isRequired,
+
 };
 
 export default ProductItem;
